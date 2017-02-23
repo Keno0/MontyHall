11ef5f73d88fbf53b31b3b62b72f29e8341504f3
Keno0 <norby185@gmail.com>
2017-02-23 13:20:08 +0100

    Monty Hall Simulation 1.0
    This is the first version of Monty Hall Simulation.

---
+++ MontyHall/Program.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MontyHall
+{
+    public class GameOfMontyHall
+    {
+        List<Door> doors;
+        Random rand = new Random();
+        PlayerOfMontyHall player;
+        int numberOfGames = 0;
+
+        public List<Door> Doors
+        {
+            get { return doors; }
+        }
+
+        public PlayerOfMontyHall Player
+        {
+            get { return player; }
+        }
+            
+        public GameOfMontyHall(int numberOfDoors, int numberOfGames, PlayerOfMontyHall player)
+        {
+            doors = new List<Door>(numberOfDoors);
+            this.numberOfGames = numberOfGames;
+            this.player = player;
+
+
+            for (int i = 0; i < numberOfDoors; i++)
+            {
+                doors.Add(new Door());
+            }
+            
+
+        }
+        public void DoorInit()
+        {
+
+            bool doorIsAlreadySet = false;
+
+            for(int i=0; i<doors.Count; i++)
+            {
+                doors[i].BehindTheDoor = 0;
+                doors[i].IsDoorOpen = false;
+                doors[i].IsSelectedByPlayer = false;
+            }
+
+            while (!doorIsAlreadySet)
+            {
+                for (int i = 0; i < doors.Count; i++)
+                {
+                    
+                    if (rand.Next(0, 1000)  == 510)
+                    {
+                        doors[i].BehindTheDoor = 1;
+                        doorIsAlreadySet = true;
+                    }
+                    doors[i].IsDoorOpen = false;
+                    doors[i].IsSelectedByPlayer = false;
+
+                    if (doorIsAlreadySet)
+                        break;
+                }
+            }
+        }
+        
+        public void SelectRandomDoor()
+        {
+            player.SelectedDoor = rand.Next(1, 4);
+            doors[player.SelectedDoor - 1].IsSelectedByPlayer = true;
+        }
+
+        public void OpenRandomDoor()
+        {
+            int indexOfSelectedDoor = 0;
+            int indexOfCarDoor = 0;
+            bool doorIsAlreadyOpened = false;
+
+            FindIndexesOfSelectedAndCarDoor(ref indexOfSelectedDoor, ref indexOfCarDoor);
+
+            if (indexOfCarDoor != indexOfSelectedDoor)
+            {
+                for (int i = 0; i < doors.Count; i++)
+                {
+                    if (i != indexOfSelectedDoor && i != indexOfCarDoor)
+                    {
+                        doors[i].OpenTheDoor();
+                    }
+                }
+            }
+            else
+            {
+                while (!doorIsAlreadyOpened)
+                {
+                    for (int i = 0; i < doors.Count; i++)
+                    {
+                        if (i != indexOfSelectedDoor && !doorIsAlreadyOpened)
+                        {
+                            if (rand.Next(0, 100) == 50)
+                            {
+                                doors[i].OpenTheDoor();
+                                doorIsAlreadyOpened = true;
+                            }
+
+                        }
+                    }
+                }
+            }
+
+        }
+
+        private void FindIndexesOfSelectedAndCarDoor(ref int indexOfSelectedDoor, ref int indexOfCarDoor)
+        {
+            for (int i = 0; i < doors.Count; i++)
+            {
+                if (doors[i].IsSelectedByPlayer)
+                    indexOfSelectedDoor = i;
+                if (doors[i].BehindTheDoor == 1)
+                    indexOfCarDoor = i;
+            }
+        }
+
+        public bool PlayerWonTheGame()
+        {
+            int indexOfCarDoors = 0;
+            
+            for (int i = 0; i < doors.Count; i++)
+            {
+                if (doors[i].BehindTheDoor == 1)
+                    indexOfCarDoors = i;
+            }
+            if(player.SelectedDoor == (indexOfCarDoors + 1))
+            {
+                player.NumberOfGamesWon++;
+                return true;
+            }
+
+            return false;
+
+        }
+
+        public void Start()
+        {
+            for (int i = 0; i < numberOfGames; i++)
+            {
+
+                DoorInit();
+                SelectRandomDoor();
+                OpenRandomDoor();
+                player.ChangeTheSelectedDoorByPlayer(doors);
+                PlayerWonTheGame();
+            }
+        }
+    }
+
+    public class PlayerOfMontyHall
+    {
+        int selectedDoor = 0;
+        int numberOfGamesWon = 0;
+
+        public int NumberOfGamesWon
+        {
+            get { return numberOfGamesWon; }
+            set { numberOfGamesWon = value; }
+        }
+        
+        public int SelectedDoor
+        {
+            get { return selectedDoor; }
+            set { selectedDoor = value; }
+        }
+
+        public virtual void ChangeTheSelectedDoorByPlayer(List<Door> doors) { }
+        
+    }
+
+    public class PlayerWhoDontChangeTheSelectedDoor: PlayerOfMontyHall
+    {
+
+    }
+
+    public class PlayerWhoChangeTheSelectedDoor: PlayerOfMontyHall
+    {
+        public override void ChangeTheSelectedDoorByPlayer(List<Door> doors)
+        {
+            bool selectedDoorIsAlreadyChanged = false;
+            for(int i=0; i< doors.Count;i++)
+            {
+                if(!doors[i].IsDoorOpen && !selectedDoorIsAlreadyChanged)
+                {
+                    if (SelectedDoor != (i + 1))
+                    {
+                        doors[SelectedDoor - 1].IsSelectedByPlayer = false;
+                        SelectedDoor = i + 1;
+                        doors[i].IsSelectedByPlayer = true;
+                        selectedDoorIsAlreadyChanged = true;
+                    }
+                }
+            }
+        }
+    }
+
+    public class Door
+    {
+        int behindTheDoor = 0;
+        bool isDoorOpen = false;
+        bool isSelectedByPlayer = false;
+
+        public bool IsSelectedByPlayer
+        {
+            get { return isSelectedByPlayer; }
+            set { isSelectedByPlayer = value; }
+        }
+
+        public bool IsDoorOpen
+        {
+            get { return isDoorOpen; }
+            set { isDoorOpen = value; }
+        }
+
+        public void OpenTheDoor()
+        {
+            isDoorOpen = true;
+        }
+
+        public int BehindTheDoor
+        {
+            get { return behindTheDoor; }
+            set { behindTheDoor = value; }
+        }
+
+  
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameOfMontyHall game1 = new GameOfMontyHall(3, 1000, new PlayerWhoChangeTheSelectedDoor());
+            GameOfMontyHall game2 = new GameOfMontyHall(3, 1000, new PlayerWhoDontChangeTheSelectedDoor());
+            game1.Start();
+            game2.Start();
+            Console.WriteLine("Player who switched his choice: " + game1.Player.NumberOfGamesWon / 10 + "%");
+            Console.WriteLine("Player who didn't switch his choice: " +  game2.Player.NumberOfGamesWon / 10+"%");
+            
+        }
+    }
+}
---
+++ MontyHallTests/DoorTests.cs
@@ -0,0 +1,260 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MontyHall;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MontyHall.Tests
+{
+    [TestClass()]
+    public class DoorTests
+    {
+        [TestMethod()]
+        public void OpenTheDoorTest()
+        {
+            Door door = new Door();
+            door.OpenTheDoor();
+
+            Assert.AreEqual(true, door.IsDoorOpen);
+
+        }
+
+        [TestMethod()]
+        public void DoorInitTest()
+        {
+            GameOfMontyHall game = new GameOfMontyHall(3, 1000, new PlayerWhoDontChangeTheSelectedDoor());
+            int carIsBehindTheFirstDoorCounter = 0;
+            int carIsBehindTheSecondDoorCounter = 0;
+            int carIsBehindTheThirdDoorCounter = 0;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                game.DoorInit();                
+
+                if (game.Doors[0].BehindTheDoor == 1)
+                    carIsBehindTheFirstDoorCounter++;
+                if (game.Doors[1].BehindTheDoor == 1)
+                    carIsBehindTheSecondDoorCounter++;
+                if (game.Doors[2].BehindTheDoor == 1)
+                    carIsBehindTheThirdDoorCounter++;
+            }
+
+            Console.WriteLine("RandomDoorValuesTest1: " +
+                carIsBehindTheFirstDoorCounter + " " +
+                carIsBehindTheSecondDoorCounter + " " +
+                carIsBehindTheThirdDoorCounter + "\n");
+
+            Assert.IsTrue(carIsBehindTheFirstDoorCounter > 300 &&
+                          carIsBehindTheSecondDoorCounter > 300 &&
+                          carIsBehindTheThirdDoorCounter > 300);
+        }
+
+        [TestMethod()]
+        public void RandomDoorValuesTest1()
+        {
+            GameOfMontyHall game = new GameOfMontyHall(3,1000, new PlayerWhoDontChangeTheSelectedDoor());
+            int carIsBehindTheFirstDoorCounter = 0;
+            int carIsBehindTheSecondDoorCounter = 0;
+            int carIsBehindTheThirdDoorCounter = 0;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                game.DoorInit();                
+
+                if (game.Doors[0].BehindTheDoor == 1)
+                    carIsBehindTheFirstDoorCounter++;
+                if (game.Doors[1].BehindTheDoor == 1)
+                    carIsBehindTheSecondDoorCounter++;
+                if (game.Doors[2].BehindTheDoor == 1)
+                    carIsBehindTheThirdDoorCounter++;
+            }
+
+            Console.WriteLine("RandomDoorValuesTest1: " +
+                carIsBehindTheFirstDoorCounter + " " +
+                carIsBehindTheSecondDoorCounter + " " +
+                carIsBehindTheThirdDoorCounter + "\n");
+
+            Assert.IsTrue(carIsBehindTheFirstDoorCounter > 300 &&
+                          carIsBehindTheSecondDoorCounter > 300 &&
+                          carIsBehindTheThirdDoorCounter > 300);
+        }
+
+        [TestMethod()]
+        public void SelectRandomDoorTest1()
+        {
+            GameOfMontyHall game = new GameOfMontyHall(3, 1000, new PlayerWhoDontChangeTheSelectedDoor());
+            int firstDoorIsSelectedCounter = 0;
+            int secondDoorIsSelectedCounter = 0;
+            int thirdDoorIsSelectedCounter = 0;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                game.SelectRandomDoor();
+                switch (game.Player.SelectedDoor)
+                {
+                    case 1:
+                        firstDoorIsSelectedCounter++;
+                        break;
+                    case 2:
+                        secondDoorIsSelectedCounter++;
+                        break;
+                    case 3:
+                        thirdDoorIsSelectedCounter++;
+                        break;
+                    default:
+                        break;
+                }                
+
+            }
+            Console.WriteLine("SelectRandomDoorTest1: " + 
+                firstDoorIsSelectedCounter + " " + 
+                secondDoorIsSelectedCounter + " " + 
+                thirdDoorIsSelectedCounter + "\n");
+
+            Assert.IsTrue(firstDoorIsSelectedCounter > 300 &&
+                              secondDoorIsSelectedCounter > 300 &&
+                              thirdDoorIsSelectedCounter > 300);
+        }
+
+
+        [TestMethod()]
+        public void OpenRandomDoorTest1()
+        {
+            GameOfMontyHall game = new GameOfMontyHall(3,1000, new PlayerWhoChangeTheSelectedDoor());
+            int firstDoorIsOpen = 0;
+            int secondDoorIsOpen = 0;
+            int thirdDoorIsOpen = 0;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                game.DoorInit();
+                game.SelectRandomDoor();
+                game.OpenRandomDoor();
+
+
+                if (game.Doors[0].IsDoorOpen == true)
+                    firstDoorIsOpen++;
+                if (game.Doors[1].IsDoorOpen == true)
+                    secondDoorIsOpen++;
+                if (game.Doors[2].IsDoorOpen == true)
+                    thirdDoorIsOpen++;
+
+            }
+
+            Console.WriteLine("OpenRandomDoorTest1: " + firstDoorIsOpen + " " + secondDoorIsOpen + " "+ thirdDoorIsOpen +"\n");
+
+            Assert.IsTrue(firstDoorIsOpen > 300 &&
+                              secondDoorIsOpen > 300 &&
+                              thirdDoorIsOpen > 300);
+        }
+
+        [TestMethod()]
+        public void PlayerWonOneGameTest()
+        {
+            GameOfMontyHall game = new GameOfMontyHall(3,1000, new PlayerWhoDontChangeTheSelectedDoor());
+            int indexOfCarDoors = 0;
+            game.DoorInit();
+            for(int i=0; i< game.Doors.Count; i++)
+            {
+                if (game.Doors[i].BehindTheDoor == 1)
+                    indexOfCarDoors = i;
+            }
+
+            game.Player.SelectedDoor = indexOfCarDoors+1;
+            game.PlayerWonTheGame();
+            Assert.AreEqual(1, game.Player.NumberOfGamesWon);
+            
+            
+        }
+
+        [TestMethod()]
+        public void StartTheGame_PlayerDontChangesSelectedDoorTest()
+        {
+            GameOfMontyHall game = new GameOfMontyHall(3, 1000, new PlayerWhoDontChangeTheSelectedDoor());
+            game.Start();
+            Console.WriteLine("NumberOfGamesWon: " + game.Player.NumberOfGamesWon);
+            Assert.IsTrue(game.Player.NumberOfGamesWon < 400);
+        }
+
+        [TestMethod()]
+        public void ChangeTheSelectedDoorByPlayerTest()
+        {
+            List<Door> doors =new List<Door>(3);
+            PlayerWhoChangeTheSelectedDoor player = new PlayerWhoChangeTheSelectedDoor();
+            doors.Add(new Door());
+            doors.Add(new Door());
+            doors.Add(new Door());
+            doors[0].OpenTheDoor();
+            player.SelectedDoor = 2;
+            player.ChangeTheSelectedDoorByPlayer(doors);
+
+            Assert.AreEqual(3, player.SelectedDoor);
+        }
+
+        [TestMethod()]
+        public void ChangeTheSelectedDoorByPlayer1000TimesTest()
+        {
+            GameOfMontyHall game = new GameOfMontyHall(3, 1000, new PlayerWhoChangeTheSelectedDoor());
+
+            int firstDoorIsSelectedCounter = 0;
+            int secondDoorIsSelectedCounter = 0;
+            int thirdDoorIsSelectedCounter = 0;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                game.DoorInit();
+                game.SelectRandomDoor();
+                game.OpenRandomDoor();
+                game.Player.ChangeTheSelectedDoorByPlayer(game.Doors);
+
+                switch (game.Player.SelectedDoor)
+                {
+                    case 1:
+                        firstDoorIsSelectedCounter++;
+                        break;
+                    case 2:
+                        secondDoorIsSelectedCounter++;
+                        break;
+                    case 3:
+                        thirdDoorIsSelectedCounter++;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine("SelectRandomDoorTest1: " +
+                firstDoorIsSelectedCounter + " " +
+                secondDoorIsSelectedCounter + " " +
+                thirdDoorIsSelectedCounter + "\n");
+
+            Assert.IsTrue(firstDoorIsSelectedCounter > 300 &&
+                              secondDoorIsSelectedCounter > 300 &&
+                              thirdDoorIsSelectedCounter > 300);
+        }
+
+        [TestMethod()]
+        public void StartTheGame_PlayerChangesSelectedDoorTest1()
+        {
+            GameOfMontyHall game = new GameOfMontyHall(3, 1000, new PlayerWhoChangeTheSelectedDoor());
+            game.Start();
+            Console.WriteLine("NumberOfGamesWon: " + game.Player.NumberOfGamesWon);
+            Assert.IsTrue(game.Player.NumberOfGamesWon > 600);
+            
+        }
+
+        [TestMethod()]
+        public void StartTheGame_PlayerChangesSelectedDoorTest2()
+        {
+            GameOfMontyHall game = new GameOfMontyHall(3, 10000, new PlayerWhoChangeTheSelectedDoor());
+            game.Start();
+            Console.WriteLine("NumberOfGamesWon: " + game.Player.NumberOfGamesWon);
+            Assert.IsTrue(game.Player.NumberOfGamesWon > 6000);
+        }
+
+
+
+    }
+}
